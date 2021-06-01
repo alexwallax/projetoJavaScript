a562e7af1f1136444f226f4db46b6a4bf550a9df
alexw <alexw@DESKTOP-LM375RU>
2021-06-01 12:15:14 -0300

    commit 03

---
+++ public_html/js/main.js
@@ -0,0 +1,56 @@
+/*
+//variaveis
+var nome = "Alex Wallace";
+var idade = 38;
+var idade2 = 2;
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+alert(frase.replace("Japão", "Brasil"));
+alert(frase.replace("Japão", "Brasil"));
+*/
+//---------------------------------------------------------------------
+//aray
+//
+//
+//var lista = ["maçã", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop("uva");
+//console.log(lista);
+//alert(lista.length);
+//alert(lista.toString()[0]);
+
+/*
+var fruta = {nome:"maçã", cor:"vermelha"}
+//console.log(fruta);
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+/*
+var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"pêra", cor:"verde"}]
+console.log(frutas);
+//console.log(frutas.nome);
+alert(frutas[1].nome);
+*/
+//---------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
